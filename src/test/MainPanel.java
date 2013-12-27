80c02e9f78d0903d1db8867a33ed79336001bf78
wangtaikim <wangtai.kim@gmail.com>
2013-12-27 22:37:11 +0900

    add listener

--- src/test/MainPanel.java
+++ src/test/MainPanel.java
@@ -1,7 +1,8 @@
 package test;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Logger;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -21,9 +22,20 @@ public class MainPanel {
 	private	JButton stopJButton;
 	private	JButton resetJButton;
 	
+	private Logger logger;
+	
 	public static void main(String[] args) {
 		MainPanel mainPanel = new MainPanel();
-		mainPanel.initPanel();
+		mainPanel.initLogger();
+		mainPanel.go();
+	}
+	
+	public void initLogger() {
+		logger = Logger.getLogger(MainPanel.class.getName());
+	}
+	
+	public void go() {
+		initPanel();
 	}
 	
 	public void initPanel() {
@@ -37,6 +49,10 @@ public class MainPanel {
 		stopJButton = new JButton(STOP);
 		resetJButton = new JButton(RESET);
 		
+		startJButton.addActionListener(startAction);
+		stopJButton.addActionListener(stopAction);
+		resetJButton.addActionListener(resetAction);
+		
 		jPanelHorizontal.add(startJButton);
 		jPanelHorizontal.add(stopJButton);
 		jPanelHorizontal.add(resetJButton);
@@ -46,6 +62,44 @@ public class MainPanel {
 		jFrame.getContentPane().add(jPanelVertical);
 		jFrame.setSize(500, 300);
 		jFrame.setVisible(true);
-	}
+		
+	}	// end initPanel
+	
+	private ActionListener startAction = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			logger.info("startAction");
+			startJButton.setEnabled(false);
+			stopJButton.setEnabled(true);
+			resetJButton.setEnabled(true);
+			
+		}
+	};	// end startAction 
+	
+	private ActionListener stopAction = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			logger.info("stopAction");
+			startJButton.setEnabled(true);
+			stopJButton.setEnabled(false);
+			resetJButton.setEnabled(true);
+			
+		}
+	};	// end stopAction 
+	
+	private ActionListener resetAction = new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			logger.info("resetAction");
+			startJButton.setEnabled(true);
+			stopJButton.setEnabled(false);
+			resetJButton.setEnabled(true);
+		}
+	}; 	// end resetAction
+	
+
 	
 }
